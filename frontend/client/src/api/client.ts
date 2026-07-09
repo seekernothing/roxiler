bbd5bd7c7d0d21a501822612367d7dca86171b04
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2026-07-10 05:15:48 +0530

    feat: initialize axios instance for API requests with base URL and credentials

--- frontend/client/src/api/client.ts
+++ frontend/client/src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7777/api",
-  withCredentials: true, // cookie ke liye ZAROORI
+  baseURL: import.meta.env.VITE_API_URL ,
+  withCredentials: true, 
 });
 
 export default api;

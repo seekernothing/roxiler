89e4bfb43d4ebc9d7acab110e3505c7c6466c527
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2026-07-10 00:55:12 +0530

    added redux store with slice

---
+++ frontend/client/src/store/authSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { User } from "../types";
+
+interface AuthState {
+  user: User | null;
+  loading: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,
+  loading: true, 
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    
+    setUser(state, action: PayloadAction<User>) {
+      state.user = action.payload;
+      state.loading = false;
+    },
+    
+    clearUser(state) {
+      state.user = null;
+      state.loading = false;
+    },
+  },
+});
+
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
